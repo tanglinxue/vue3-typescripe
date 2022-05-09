ad66944acfa6190e8ff5d1e18d5e15d24a0ffcfb
tanglinxue <31237473+tanglinxue@users.noreply.github.com>
2022-05-09 17:29:05 +0800

    first commit

---
+++ 01源码/App.vue
@@ -0,0 +1,32 @@
+<template>
+<!--Vue2中的html模版中必须要有一对根标签,Vue3组件的html模版中可以没有根标签-->
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+// 这里可以使用ts的代码
+
+// defineComponent函数,目的是定义一个组件,内部可以传入一个配置对象
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+// 暴露出去一个定义好的组件
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ref使用/App.vue
@@ -0,0 +1,32 @@
+<template>
+   <h3>{{ count }}</h3>
+  <button @click="updateCount">更新数据</button>
+</template>
+
+<script lang="ts">
+
+
+// defineComponent函数,目的是定义一个组件,内部可以传入一个配置对象
+import { defineComponent,ref} from 'vue';
+ // ref是一个函数,作用:定义一个响应式的数据,返回的是一个Ref对象,对象中有一个value属性,如果需要对数据进行操作,
+ //需要使用该Ref对象调用value属性的方式进行数据的操作
+
+// 暴露出去一个定义好的组件
+export default defineComponent({
+  name: 'App',
+  setup(){
+    const count = ref(0)
+
+    function updateCount(){
+      count.value++
+    }
+
+    return {
+      count,
+      updateCount
+    }
+  }
+});
+</script>
+
+
